8450f624f086c691595e918796c978c106c842ba
HoshkoS <sofochka9500ho@gmail.com>
2023-11-06 19:40:43 +0200

    Connect to database

---
+++ ThinkTwice/ThinkTwice/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThinkTwice.Models;
+
+public partial class Category
+{
+    public Guid Id { get; set; }
+
+    public Guid? UserId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public bool IsGeneral { get; set; }
+
+    public decimal PercentageAmount { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public virtual ICollection<Transaction> TransactionFromCategoryNavigations { get; set; } = new List<Transaction>();
+
+    public virtual ICollection<Transaction> TransactionToCategoryNavigations { get; set; } = new List<Transaction>();
+
+    public virtual User? User { get; set; }
+}
---
+++ ThinkTwice/ThinkTwice/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThinkTwice.Models;
+
+public partial class Transaction
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public Guid? FromCategory { get; set; }
+
+    public Guid? ToCategory { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Details { get; set; }
+
+    public bool Planned { get; set; }
+
+    public virtual Category? FromCategoryNavigation { get; set; }
+
+    public virtual Category? ToCategoryNavigation { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
